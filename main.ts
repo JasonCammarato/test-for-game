2ac0c420bbbbc02ecbcc92987921eddfa47a09a9
JasonCammarato <122560123+JasonCammarato@users.noreply.github.com>
2023-01-13 13:29:56 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showString("Hi user!")
+basic.pause(100)
+basic.showLeds(`
+    . . # # .
+    . . # . #
+    # . # . #
+    . . # . #
+    # . # # .
+    `)
+music.playMelody("E - - G A - - C ", 120)
