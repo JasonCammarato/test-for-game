e76ab48e06f8fe6d1d5e378e9ed4ec5e085617bb
JasonCammarato <122560123+JasonCammarato@users.noreply.github.com>
2023-01-19 13:27:52 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,185 @@
-basic.showString("Hi user!")
-basic.pause(100)
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # . . . .
+        # # . # #
+        # . . # .
+        . . . . .
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        # # . # #
+        . # . # .
+        . . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        # # . # #
+        . # . # .
+        . . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        # # . # #
+        . # . # .
+        . . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        # # . # #
+        . # . # .
+        . . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # . # .
+        . # . # #
+        . . . # .
+        . . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # . # .
+        . # . # #
+        . . . # .
+        . . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # . # .
+        . # . # #
+        . . . # .
+        . . . . .
+        # # # # #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . #
+        # # . # #
+        . # . . #
+        . . . . .
+        # # # # #
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . . #
+        . . . . .
+        # # # # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        . # . . #
+        . . . . .
+        . . . . .
+        # # # # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . # .
+        . . . . .
+        . . . . .
+        # # # # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . # .
+        . . . . .
+        # # # # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # . . #
+        . . . . .
+        # # # # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        . # . . #
+        . . . . .
+        . . . . .
+        # # # # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        # . . . #
+        . . . . .
+        # # # # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        # # . # #
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        # # . # #
+        . . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        # . # . .
+        . . . . .
+        # # . # #
+        . . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        # . # . #
+        . . . . .
+        # # . # #
+        . . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # . # #
+        . . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        # # . # #
+        . . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        # . # . .
+        . . . . .
+        # # . # #
+        . . . . .
+        # # # # #
+        `)
+    basic.showLeds(`
+        # . # . #
+        . . . . .
+        # # . # #
+        . . . . .
+        # # # # #
+        `)
+})
 basic.showLeds(`
-    . . # # .
-    . . # . #
-    # . # . #
-    . . # . #
-    # . # # .
+    # # . # #
+    # # . # #
+    . . . . .
+    # # # # #
+    . # # # .
     `)
-music.playMelody("E - - G A - - C ", 120)
